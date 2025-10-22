00a28f9de55f0890a60be6560734fe3e6cc944e1
Josep M Sobrepere <jm.sobrepere@gmail.com>
2025-10-22 21:46:00 +0200

    fix stuff

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ if (Number.isNaN(latestIndexedEra)) {
   latestIndexedEra = activeEra - depth
 }
 
-const nMissing = activeEra - latestIndexedEra // - 1
+const nMissing = activeEra - latestIndexedEra - 1
 console.log(`There are ${nMissing} era(s) that will be indexed`)
 
 for (let i = 0; i <= nMissing; i++) {
